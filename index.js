ea57c6628f6a018a47cdbaeb1e5a61ac582c86f0
Abhishek <abhishek.mittal.msc@gmail.com>
2023-05-10 18:11:28 +0530

    'changes'

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use(cors());
 
 app.use('/api', postRoutes);
 
-const CONNECTION_URL = 'mongodb://0.0.0.0:27017';
+const CONNECTION_URL = 'mongodb+srv://abhishekmittalmsc:Welcome@12345@cluster0.3edhtcf.mongodb.net/?retryWrites=true&w=majority';
 const PORT = process.env.PORT|| 5000;
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
